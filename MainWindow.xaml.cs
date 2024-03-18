ded8452cd92bdb040a450983f71c4f4343cddbe8
Levi-24 <leventemajor36@gmail.com>
2024-03-18 11:19:14 +0100

    Complete

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,47 @@ namespace SzemelyisegTeszt
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SubWindow subWindow = new SubWindow();
+            subWindow.Show();
+
+            string str1 = "";
+            string str2 = "";
+            string str3 = "";
+            string str4 = "";
+            string str5 = "";
+
+            foreach (CheckBox checkBox in stackp1.Children)
+            {
+                if (checkBox.IsChecked == true) str1 += Convert.ToString(checkBox.Content) + ",";
+                subWindow.lbl1.Content = str1;
+            }
+
+            foreach (CheckBox checkBox in stackp2.Children)
+            {
+                if (checkBox.IsChecked == true) str2 += Convert.ToString(checkBox.Content) + ",";
+                subWindow.lbl2.Content = str2;
+            }
+
+            foreach (CheckBox checkBox in stackp3.Children)
+            {
+                if (checkBox.IsChecked == true) str3 += Convert.ToString(checkBox.Content) + ",";
+                subWindow.lbl3.Content = str3;
+            }
+
+            foreach (CheckBox checkBox in stackp4.Children)
+            {
+                if (checkBox.IsChecked == true) str4 += Convert.ToString(checkBox.Content) + ",";
+                subWindow.lbl4.Content = str4;
+            }
+
+            foreach (CheckBox checkBox in stackp5.Children)
+            {
+                if (checkBox.IsChecked == true) str5 += Convert.ToString(checkBox.Content) + ",";
+                subWindow.lbl5.Content = str5;
+            }
+        }
     }
 }
