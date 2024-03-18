a7888b7f43ddc4d7e2c2fb610b6b17c8bac8479e
Levi-24 <leventemajor36@gmail.com>
2024-03-18 10:13:43 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SzemelyisegTeszt
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
